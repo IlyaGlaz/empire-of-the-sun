0f21d3211c0bee4dc78a5e54dfa4acf7141b15d9
IlyaGlaz <glazterix@gmail.com>
2026-03-07 21:18:54 +0300

    play.js and rules.js template and assets

---
+++ play.js
@@ -0,0 +1,15 @@
+/*
+"use strict"
+function send_action(verb, noun) {}
+function action_button(verb, label) {}
+function action_button_with_argument(verb, noun, label) {}
+function confirm_send_action(verb, noun, question) {}
+function confirm_action_button(verb, label, question) {}
+function send_query(what) {}
+
+function on_init(scenario, options, static_view) {}
+function on_update() {}
+function on_prompt(text) {}
+function on_log(text) {}
+function on_reply(what, response) {}
+*/
